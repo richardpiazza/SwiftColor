dc207cfb3f366398c5b25d1d751575f8736da489
Richard Piazza <richard@richardpiazza.com>
2020-11-30 14:25:57 -0600

    Updated Linux testing manifest.

--- Tests/SwiftColorTests/XCTestManifests.swift
+++ Tests/SwiftColorTests/XCTestManifests.swift
@@ -3,7 +3,7 @@ import XCTest
 #if !canImport(ObjectiveC)
 public func allTests() -> [XCTestCaseEntry] {
     return [
-        testCase(SwiftColorTests.allTests),
+        testCase(ColorTests.allTests),
     ]
 }
 #endif
